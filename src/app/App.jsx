80c9b83f4422cb62f3d54d372a2cbb25962fc890
Komal Priya <kryptonbenzene@gmail.com>
2026-05-04 22:09:33 +0530

    updated code

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,34 @@
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Layout from "../components/Layout/Layout";
+// import { PlayerProvider } from "../context/PlayerContext";
+// import ArtistPage from "../pages/ArtistPage";
+// import Assistant from "../pages/Assistant";
+// import Home from "../pages/Home";
+// import Library from "../pages/Library";
+// import PlaylistDetail from "../pages/PlaylistDetail";
+// import Search from "../pages/Search";
+
+// function App() {
+//   return (
+//     <PlayerProvider>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route element={<Layout />}>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/search" element={<Search />} />
+//             <Route path="/library" element={<Library />} />
+//             <Route path="/assistant" element={<Assistant />} />
+//             <Route path="/artist/:name" element={<ArtistPage />} />
+//             <Route path="/playlist/:id" element={<PlaylistDetail />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </PlayerProvider>
+//   );
+// }
+
+// export default App;
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 import { PlayerProvider } from "../context/PlayerContext";
@@ -7,8 +38,17 @@ import Home from "../pages/Home";
 import Library from "../pages/Library";
 import PlaylistDetail from "../pages/PlaylistDetail";
 import Search from "../pages/Search";
+import { handleGoogleRedirect } from "../lib/firebase";
 
 function App() {
+  useEffect(() => {
+    handleGoogleRedirect().then((user) => {
+      if (user) {
+        console.log("Google login success:", user);
+      }
+    });
+  }, []);
+
   return (
     <PlayerProvider>
       <BrowserRouter>
