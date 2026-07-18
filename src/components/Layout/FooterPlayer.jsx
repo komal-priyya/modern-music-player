ed4f06a04a1f56103334b0262e203cfe3d384d29
Komal Priya <kryptonbenzene@gmail.com>
2026-07-18 23:30:19 +0530

    deleted the unwanted code

--- src/components/Layout/FooterPlayer.jsx
+++ src/components/Layout/FooterPlayer.jsx
@@ -1,45 +1,4 @@
-// import { Outlet } from "react-router-dom";
-// import FooterPlayer from "./FooterPlayer";
-// import Header from "./Header";
-// import PageNavigation from "./PageNavigation";
-// import Sidebar from "./Sidebar";
 
-// function Layout() {
-//   return (
-//     <div className="flex min-h-screen w-full overflow-x-hidden bg-transparent text-slate-50">
-//       <Sidebar />
-
-//       <div className="flex min-h-screen min-w-0 flex-1 flex-col">
-//         <Header />
-
-//         <main
-//           className="
-//             flex-1
-//             px-2
-//             pt-16
-//             pb-56
-//             sm:px-4
-//             sm:pt-20
-//             sm:pb-52
-//             md:px-6
-//             md:pb-40
-//             lg:px-8
-//             xl:pt-6
-//           "
-//         >
-//           <div className="mx-auto w-full max-w-7xl overflow-hidden">
-//             <PageNavigation />
-//             <Outlet />
-//           </div>
-//         </main>
-
-//         <FooterPlayer />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Layout;
 
 import { useEffect, useRef } from "react";
 import { Pause, Play, SkipBack, SkipForward, Volume2, Waves } from "lucide-react";
